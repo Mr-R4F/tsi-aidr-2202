528c7b068994552181d9ee61297522a920a0a75f
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2022-10-28 21:12:53 -0300

    subindo

--- 21-10/gameapp/src/app/service/jogo.service.ts
+++ 21-10/gameapp/src/app/service/jogo.service.ts
@@ -17,24 +17,31 @@ export class JogoService { //Todos os serviços são classes
 
   constructor() { } //A definição do serviço fica aqui
 
-  inserir(jogo: Jogo) { //Para inserir o jogo (recebe um jogo com parametro e joga no array) (passar parâmetro e tipo) //CREATE
+  inserir(jogo?: Jogo) { //Para inserir o jogo (recebe um jogo com parametro e joga no array) (passar parâmetro e tipo) //CREATE
+    if (!jogo) return; //verifica se tem valor senão volta
     jogo.id = this.i; //joga no id da array;
     this.jogos.push(jogo); //joga itens sempre no final de um array
     //gerar um id manual (contador nesse caso)
     this.i++;
+    this.salvar();
   }
 
-  listar(): Jogo[] { //READ
+  listar(): Jogo[] { //READ //antes de listar dá o return no array
+    this.ler();
     return this.jogos; //apenas retorna o array //volta o array (não é necessário colocar o tipo do retorno pois o TS difere)
   }
 
-  atualizar(jogo: Jogo) { //receber o array com o id atualizado //UPDATE
+  atualizar(jogo?: Jogo) { //receber o array com o id atualizado //UPDATE
+    if (!jogo) return; //se for nulo n faz nada //o serviço valida
     //passar o jogo tendo todos os dados (mesmo que nao haja alteração, inclusive o ID) (procura o id mostra as infos e atualiza de acordo c/ a posição)
     //findIndex -> condição e verifica uma condição -> executa a com base na condição e retorna a posição (do primeiro elemento) na qual a condição se satisfaz
     const index = this.jogos.findIndex(j => j.id == jogo.id); //se for igual executa
 
     //verificar se é -1 (pois não há como alterar)
-    if (index >= 0) this.jogos[index] = jogo;
+    if (index >= 0) {
+      this.jogos[index] = jogo;
+      this.salvar();
+    }
   }
 
   remover(i: number) { //REMOVER
@@ -43,21 +50,31 @@ export class JogoService { //Todos os serviços são classes
     this.jogos = this.jogos.filter(jogo => jogo.id != i) //representa um item na posição (param de cd jogo passado na função) (e o id passado por parametro)
     //diferente fica, senão sai
     //joga no array o val do array.
+    this.salvar();
   }
 
   //Após isso criar um componenete para "conversar" c/ ISSO.
-  
 
 
+  /* FONTE DE DADOS GRAVADO NA MEMÓRIA */
 
+  //LOCAL STORAGE
 
+  salvar() { //a cada operação os dados persistem
+    const DADOS_JSON = JSON.stringify(this.jogos);
+    localStorage.setItem('Jogos', DADOS_JSON);
+    localStorage.setItem('id', this.i.toString());
+    //grava o valor para continuar da onde parou
+  }
 
+  ler() { //fazer a função de leitura antes de ler algo
+    const DADOS_JSON = localStorage.getItem('Jogos');
+    if (DADOS_JSON) this.jogos = JSON.parse(DADOS_JSON); //se ao acaso for nulo (quer dizer que é a primeira vez que roda, senão terá valor)
 
-
-
-
-
-
+    //retorna id
+    const ID = localStorage.getItem('id');
+    if(ID) this.i = Number(ID); //se ao acaso for nulo (quer dizer que é a primeira vez que roda, senão terá valor)
+  }
 
 }
 
--- 21-10/gameapp/src/app/views/jogo/jogo.component.ts
+++ 21-10/gameapp/src/app/views/jogo/jogo.component.ts
@@ -8,26 +8,14 @@ import { JogoService } from 'src/app/service/jogo.service'; //necessário injeç
   styleUrls: ['./jogo.component.css']
 })
 export class JogoComponent implements OnInit {
-  jogos = new Array<Jogo>; //array p/ exibição dos dados
+  jogos = new Array<Jogo>; //array p/ exibição dos dados (para remover aqui)
   jogoSelecionado?: Jogo; //é opcional
+  estaEditando = false;
 
   //JogoService: JogoService <-- é como se fosse isso e param no construtor
   //Montar função de listagem
   //inserir manual
   constructor(private jogoService: JogoService) { //guarda os dados
-    const jogo = new Jogo();
-    jogo.nome = 'The Last of Us';
-    jogo.plataforma = 'PS4';
-    jogo.genero = 'Survival Horror';
-    jogo.status = '';
-    jogoService.inserir(jogo); //passar o jogo que quer inserir.
-
-    const jogo2 = new Jogo();
-    jogo2.nome = 'Valorant';
-    jogo2.plataforma = 'PC';
-    jogo2.genero = 'FPS';
-    jogo2.status = '';
-    jogoService.inserir(jogo2);
   } //IR AQUI (parâmetros, tipo do serviço e private) (e p/ cd coisa que o angular vai injectar, criar um param privado)(colocar algo que quer que injete) (deve estar marcada com a visibilidade)
   //necessário @injectable
 
@@ -36,10 +24,53 @@ export class JogoComponent implements OnInit {
   }
 
   atualizarLista() { //pega o que está no serviço e joga nessa var
-    this.jogos = this.jogoService.listar(); //pega os dados do serviço e joga aqui. (min - instancia da classe)
+    this.jogos = this.jogoService.listar(); //pega os dados do serviço e joga aqui. (min - instancia da classe) (está apontando para o mesmo lugar)
   }
 
   selecionarJogo(jogo: Jogo) { //é chamado toda vez que clica na lista (de cada interação)
-    this.jogoSelecionado = jogo; //joga o jogo como parâmetro
+    this.jogoSelecionado = jogo; //joga o jogo como parâmetro //DO ARRAY VE PARA CÁ (aqui nesse caso aponta para o mesmo lugar)
+    this.estaEditando = true;
   }
+  /* 28 */
+  salvar() {
+    //chama o inserir e novo a depender
+    this.estaEditando ? this.jogoService.atualizar(this.jogoSelecionado) : this.jogoService.inserir(this.jogoSelecionado); //chama o serviço para salvar a alteração.
+    this.cancelar(); //ao salvar provalvel cancelar
+  }
+
+  cancelar() {
+    this.jogoSelecionado = undefined; //joga nada / para não ter mais valor e sumir o formulário //para salvar pegar esse aqui
+    this.atualizarLista(); //reseta o array (mesmo se fez alguma alteração);
+  }/* 28 */
+
+  //criar uma novo
+
+  novo() {
+    this.jogoSelecionado = new Jogo(); //
+    this.estaEditando = false;
+  }
+
+  excluir(id?: number) { //id do jogo para remover
+    if (!id) return;
+
+    //chamar o serviço e pedir para remover
+    this.jogoService.remover(id);
+    //atualizar o lista quando remover (manda para o array)
+    this.atualizarLista();
+  }
+
+  //shallow copy - mesma variavel e as duas apontam para o mesmo lugar na memoria
+  //deep copy -> pega a memoria cria uma nova copia e joga em uma variável (assim uma não modifica a outra)
+  //flag -> faz a mudança de estados (faz o controle)
+  //toda vez que a flag atualiza o conteudo muda a depender
+  //para exclusão ao criar um item excluir do serviço
+  //toda vez que atualizar a lista chama
 }
+
+//local, session
+//o angular possui acesso
+//anteriormente era em cookie
+//array -> objeto -> objeto
+//serialização -> transforma obje em texto o inverso texto para objeto
+//componente e serviço possuem separação
+//mexer apenas no serviço //istóo é feito para ter separação
